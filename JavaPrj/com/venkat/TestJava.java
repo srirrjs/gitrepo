d8a79b8cacd934fac1a7d04aef41ccbd16568bf8
srirrjs <srirrjs@gmail.com>
2017-07-06 14:51:06 +0530

    New Sample Maven Prj

---
+++ JavaPrj/com/venkat/TestJava.java
@@ -0,0 +1,11 @@
+package com.venkat;
+public class TestJava {
+
+	public static void main(String[] args) {
+		for (int cont=0;cont<5; cont++)
+		System.out.println("TEst message number - "+cont);
+
+	}
+
+
+}
